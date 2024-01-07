2d2bca657d9d3298f01404ee17cf5ef1ae5b1a1f
RemiSaurel <r.saurel5@gmail.com>
2024-01-07 12:34:27 +0100

    comments + first routes

--- index.js
+++ index.js
@@ -1,50 +1,64 @@
-const path = require("path");
-const express = require("express");
-const session = require("express-session");
-const Keycloak = require("keycloak-connect");
-const parseToken = require("./utils").parseToken;
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const Keycloak = require('keycloak-connect');
+const parseToken = require('./utils').parseToken;
 
 const app = express();
 const memoryStore = new session.MemoryStore();
 
-app.set("view engine", "ejs");
-app.set("views", require("path").join(__dirname, "/view"));
-app.use(express.static("static"));
+//////////////////////////
+// JS CONFIG
+//////////////////////////
+app.set('view engine', 'ejs');
+app.set('views', require('path').join(__dirname, '/view'));
+app.use(express.static('static'));
+
+//////////////////////////
+// KEYCLOAK CONFIG
+//////////////////////////
 app.use(
   session({
-    secret: "auErxiQoVYpOBd04qEFitZfwmI5GLHCS",
+    secret: '**********',
     resave: false,
     saveUninitialized: true,
     store: memoryStore,
   }),
 );
-
 const keycloak = new Keycloak({ store: memoryStore });
-
 app.use(
   keycloak.middleware({
-    logout: "/logout",
+    logout: '/logout',
   }),
 );
 
-// No keycloack.protect() middleware here cause PUBLIC
-app.get("/", (req, res) => {
+//////////////////////////
+// ROUTES
+//////////////////////////
+// No keycloak.protect() middleware here because PUBLIC
+app.get('/', (req, res) => {
+  console.log('Index page');
   // Check if the user is authenticated
-  if (req.session["keycloak-token"]) {
-    return res.redirect("/home");
+  if (req.session['keycloak-token']) {
+    return res.redirect('/home');
   } else {
-    return res.redirect("/login");
+    return res.redirect('/login');
   }
 });
 
-// No keycloack.protect() middleware here cause PUBLIC
-app.get("/login", (req, res, next) => {
-  // Redirect to keycloak login page
-  return keycloak.protect()(req, res, next);
+// No keycloak.protect() middleware here because PUBLIC
+app.get('/login', (req, res, next) => {
+  console.log('Login page');
+
+  // Redirect to keycloack login page
+  // Then, if user OK -> redirect to /home
+  // Else -> redirect to / or error page with retry button
 });
 
-app.get("/home", keycloak.protect(), (req, res, next) => {
-  const details = parseToken(req.session["keycloak-token"]);
+// ROUTE PROTECTED BY KEYCLOAK
+app.get('/home', keycloak.protect(), (req, res, next) => {
+  console.log('Home page');
+  const details = parseToken(req.session['keycloak-token']);
   const embedded_params = {};
 
   if (details) {
@@ -53,44 +67,50 @@ app.get("/home", keycloak.protect(), (req, res, next) => {
     embedded_params.username = details.preferred_username;
   }
 
-  res.render("home", {
+  res.render('home', {
     user: embedded_params,
   });
 });
 
 app.get(
-  "/asset01",
-  keycloak.enforcer(["asset-01:read"], {
-    resource_server_id: "my-application",
+  '/asset01',
+  keycloak.enforcer(['asset-01:read'], {
+    resource_server_id: 'my-application',
   }),
   (req, res) => {
-    return res.status(200).end("Succès");
+    return res.status(200).end('Success');
   },
 );
 
 app.get(
-  "/asset01/update",
-  keycloak.enforcer(["asset-01:write"], {
-    resource_server_id: "my-application",
+  '/asset01/update',
+  keycloak.enforcer(['asset-01:write'], {
+    resource_server_id: 'my-application',
   }),
   (req, res) => {
-    return res.status(200).end("Succès");
+    return res.status(200).end('Success');
   },
 );
 
+//////////////////////////
+// SERVER CONFIG
+//////////////////////////
 app.use((req, res, next) => {
-  return res.status(404).end("Not Found");
+  return res.status(404).end('Not Found');
 });
 
-app.use((err, req, next) => {
+app.use((err, req, res, next) => {
   return res
-    .status(req.errorCode ? req.errorCode : 500)
-    .end(req.error ? req.error.toString() : "Internal server error");
+    .status(err.status || 500)
+    .end(err.message || 'Internal server error');
 });
 
-const server = app.listen(3000, "127.0.0.1", () => {
+//////////////////////////
+// SERVER START
+//////////////////////////
+const server = app.listen(3000, '127.0.0.1', () => {
   const host = server.address().address;
-  const post = server.address().port;
+  const port = server.address().port;
 
-  console.log(`Server listening on http://${host}:${post}`);
+  console.log(`Server listening on http://${host}:${port}`);
 });
